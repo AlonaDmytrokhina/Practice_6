1df9022699df6eb60236bcb6321324dcf85b26e0
Alona Dmytrokhina <dmitrohina1708@gmail.com>
2024-03-14 21:12:30 +0200

    Added merge sort to high-level code

--- HighLanFofPractice6.java
+++ HighLanFofPractice6.java
@@ -27,10 +27,13 @@ public static void main(String[] args) {
         }
     }
 
-    printArray(nSubInLine(text, sub));
-
+    int[][] result = nSubInLine(text, sub);
+    System.out.println("Sorted array with data(<number of occurrences><index of line in text>):");
+    mergeSort(result, 0, result.length - 1);
+    printArray(result);
 }
 
+// A method for reducing an array with strings
 private static String[] editedText(String[] text, int end){
     String[] edited = new String[end];
     for(int i=0; i<edited.length; i++){
@@ -43,33 +46,13 @@ private static String[] editedText(String[] text, int end){
 private static int[][] nSubInLine(String[] text, String sub){
     int[][] res = new int[text.length][2];
     int n = 0;
-    // int index = 0;
-    boolean check = true;
+    int index = 0;
     for(int a=0; a<text.length; a++){
         n = 0;
-        for(int i=0; i<text[a].length(); i++){
-            check = true;
-            for(int j=0; j<sub.length(); j++){
-                if(sub.charAt(j)!=text[a].charAt(i+j)){
-                    check = false;
-                    break;
-                }
-            }
-            if(check){
-                n++;
-                if(text[a].length() > i+sub.length()){
-                    i+=sub.length();
-                }
-                else{
-                    break;
-                }
-            }
-        }
-
-        /*while ((index = text[a].indexOf(sub, index)) != -1) {
+        while ((index = text[a].indexOf(sub, index)) != -1) {
             n++;
             index += sub.length();
-        }*/
+        }
 
         res[a][0] = n;
         res[a][1] = a;
@@ -78,6 +61,7 @@ private static int[][] nSubInLine(String[] text, String sub){
     return res;
 }
 
+// A method for printing an array
 public static void printArray(int[][] arr) {
     for (int i = 0; i < arr.length; i++) {
         
@@ -85,6 +69,42 @@ public static void printArray(int[][] arr) {
             System.out.print(arr[i][j] + " ");
         }
         System.out.println();
+    }
+}
+
+// A method for separation the array
+public static void mergeSort(int[][] arr, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+// This method implements the merge
+public static void merge(int[][] arr, int left, int mid, int right) {
+    int[][] temp = new int[right - left + 1][2];
+    int i = left, j = mid + 1, k = 0;
+
+    while (i <= mid && j <= right) {
+        if (arr[i][0] < arr[j][0] || (arr[i][0] == arr[j][0] && arr[i][1] <= arr[j][1])) {
+            temp[k++] = arr[i++];
+        } else {
+            temp[k++] = arr[j++];
+        }
+    }
+
+    while (i <= mid) {
+        temp[k++] = arr[i++];
+    }
+
+    while (j <= right) {
+        temp[k++] = arr[j++];
+    }
+
+    for (i = left, k = 0; i <= right; i++, k++) {
+        arr[i] = temp[k];
     }
 }
 
